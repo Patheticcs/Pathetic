569e29a9beb6b29dff9df4356e3cfb13af202884
path <patheticcs@gmail.com>
2025-01-08 13:34:39 -0800

    Update _app.js

--- _app.js
+++ _app.js
@@ -1,1 +1,12 @@
 import { Analytics } from "@vercel/analytics/react";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} /> {/* This renders your pages */}
+      <Analytics /> {/* This tracks visitors */}
+    </>
+  );
+}
+
+export default MyApp;
